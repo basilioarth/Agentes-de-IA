f0eb2287008d73e4116511344e36f23753af00e7
basilioarth <basilio.arth@gmail.com>
2025-12-13 22:56:08 -0300

    Ferramentas com parâmetros

--- 1. O que é um Agente de IA/SecretarIA/src/secretaria.js
+++ 1. O que é um Agente de IA/SecretarIA/src/secretaria.js
@@ -8,7 +8,7 @@ const contents = [
     {
         role: "user",
         parts: [{
-            text: "que dia é hoje?"
+            text: "Qual a temperatura no Brasil?"
         }]
     }
 ];
@@ -22,7 +22,25 @@ var response = await ai.models.generateContent({
                 functionDeclarations: [
                     {
                         name: "getTodayDate",
-                        description: "Retorna a data de hoje no formato yyyy-mm-dd"
+                        description: "Retorna a data de hoje no formato dd/mm/yyyy"
+                    },
+                    {
+                        name: "getCountryTemperature",
+                        description: "Retorna a temperatura do país indicado",
+                        parameters: {
+                            type: "OBJECT",
+                            properties: {
+                                country: {
+                                    type: "STRING",
+                                    description: "País do qual se quer saber a temperatura"
+                                },
+                                isCelsius: {
+                                    type: "BOOLEAN",
+                                    description: "Se devemos retornar a temperatura em Celsius ou não (padrão é true)"
+                                }
+                            },
+                            required: ["country", "isCelsius"]
+                        }
                     }
                 ]
             }
@@ -30,21 +48,21 @@ var response = await ai.models.generateContent({
     }
 });
 
-contents.push(response.candidates[0].content);
+// contents.push(response.candidates[0].content);
 
-contents.push({
-    role: "user",
-    parts: [{
-        functionResponse: {
-            name: "getTodayDate",
-            response: { response: "2075-05-01" }
-        }
-    }]
-});
+// contents.push({
+//     role: "user",
+//     parts: [{
+//         functionResponse: {
+//             name: "getTodayDate",
+//             response: { response: new Date().toLocaleDateString('pt-BR') }
+//         }
+//     }]
+// });
 
-response = await ai.models.generateContent({
-    model: "gemini-2.5-flash",
-    contents: contents,
-});
+// response = await ai.models.generateContent({
+//     model: "gemini-2.5-flash",
+//     contents: contents,
+// });
 
-console.log(response.candidates[0].content);
+console.log(response.candidates[0].content.parts[0].functionCall);
